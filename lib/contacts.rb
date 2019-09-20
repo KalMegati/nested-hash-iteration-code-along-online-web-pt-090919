c42cd679f31b5809b888f0a3783bdfca7c336777
Srikalyan Megati <kalmegati@gmail.com>
2019-09-20 02:20:42 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,5 +18,6 @@ def remove_strawberry(contacts)
   contacts["Freddy Mercury"][:favorite_ice_cream_flavors].each_with_index {
     |flavor, index| if flavor = "strawberry"
       contacts["Freddy Mercury"][:favorite_ice_cream_flavors].delete_at(index)
+    end
   }
 end
